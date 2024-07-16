c6ab8e85fbb3db546088b7735eb0cffd02aa89b7
gyeyeon <iam.gyeyeon@gmail.com>
2024-07-16 21:08:01 +0900

    init project

---
+++ src/main/java/com/project/datainsight/DatainsightApplication.java
@@ -0,0 +1,13 @@
+package com.project.datainsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatainsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatainsightApplication.class, args);
+	}
+
+}
